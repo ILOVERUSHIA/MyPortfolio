dc546338d63f9471830403272471179c0b9b2a27
ILOVERUSHIA <kaerurem@gmail.com>
2026-05-23 15:39:13 +0900

    0523

---
+++ clicker/Assets/script/ClickCounter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro; // TextMeshProを使用するため
+
+public class ClickCounter : MonoBehaviour
+{
+    // クリック数を表示するUIテキスト
+    public TextMeshProUGUI countText;
+
+    // クリック数をカウントする変数
+    private int clickCount = 0;
+
+    void Start()
+    {
+        // 初期状態のクリック数を表示
+        UpdateCountText();
+    }
+
+    void Update()
+    {
+        // 左クリック（マウスボタン0）された瞬間
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickCount++; // クリック数を1増やす
+            UpdateCountText(); // テキストを更新
+        }
+    }
+
+    // UIのテキストを書き換えるメソッド
+    void UpdateCountText()
+    {
+        countText.text = "click: " + clickCount.ToString();
+    }
+}
